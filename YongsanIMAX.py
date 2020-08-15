4a59d2a8962083035875c0e8ce41ec16d4c00d52
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-16 04:11:07 +0900

    Add files via upload

---
+++ YongsanIMAX.py
@@ -0,0 +1,23 @@
+import requests
+import telegram
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+my_token = '1136456443:AAHnhXqpT-NqUZiK909_5017pah7wJ6LQ04'
+bot = telegram.Bot(token = my_token)
+now = datetime.now()
+
+
+date = 20200901
+url='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=07&theatercode=0013&date=20200831&screencodes=&screenratingcode=02&regioncode=cf'
+response = requests.get(url)
+html = response.text
+while date <= 20200905:
+	if str(date) in html:
+		bot.sendMessage(chat_id = -1001299873599, text = "%d 예매오픈!" % date) 
+	else:
+		print(now)
+		print(date)
+	date = date + 1
+
+
