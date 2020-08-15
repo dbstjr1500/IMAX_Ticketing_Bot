983dfb0384a8bae6019c9ab8b358edd5f1efc213
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-16 04:36:33 +0900

    Update YongsanIMAX.py

--- YongsanIMAX.py
+++ YongsanIMAX.py
@@ -12,12 +12,12 @@ date = 20200901
 url='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=07&theatercode=0013&date=20200831&screencodes=&screenratingcode=02&regioncode=cf'
 response = requests.get(url)
 html = response.text
-while date <= 20200905:
+while date < 20200905:
 	if str(date) in html:
 		bot.sendMessage(chat_id = -1001299873599, text = "%d 예매오픈!" % date) 
 	else:
-		if(now.hour == 4 and now.minute == 25):
-			bot.sendMessage(chat_id = -1001299873599, text = "04:25 용아맥알리미 정상작동중!" % date)
+		if(now.hour == 11 and now.minute == 59):
+			bot.sendMessage(chat_id = -1001299873599, text = "%d:%d 용아맥알리미 정상작동중!" % (now.hour, now.minute))
 		print(now)
 		print(date)
 	date = date + 1
