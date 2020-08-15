a2b40e74af933d9723760c5662112e7a3e3991c7
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-16 04:21:20 +0900

    Update YongsanIMAX.py

--- YongsanIMAX.py
+++ YongsanIMAX.py
@@ -16,6 +16,8 @@ while date <= 20200905:
 	if str(date) in html:
 		bot.sendMessage(chat_id = -1001299873599, text = "%d 예매오픈!" % date) 
 	else:
+		if(now.hour == 4 and now.minute == 25):
+			bot.sendMessage(chat_id = -1001299873599, text = "04:25 용아맥알리미 정상작동중!" % date)
 		print(now)
 		print(date)
 	date = date + 1
