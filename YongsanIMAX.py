f454aec6f471c8b510e7b9dd1a228df2002c667f
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-16 04:51:08 +0900

    Update YongsanIMAX.py

--- YongsanIMAX.py
+++ YongsanIMAX.py
@@ -1,25 +1,30 @@
 import requests
 import telegram
+import time
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 my_token = '1136456443:AAHnhXqpT-NqUZiK909_5017pah7wJ6LQ04'
 bot = telegram.Bot(token = my_token)
-now = datetime.now()
-
 
 date = 20200901
+confirm_date = 20200900
 url='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=07&theatercode=0013&date=20200831&screencodes=&screenratingcode=02&regioncode=cf'
 response = requests.get(url)
 html = response.text
-while date < 20200905:
-	if str(date) in html:
+while True:
+	now = datetime.now()
+	if date == 20200905:
+		date = confirm_date + 1
+		time.sleep(5)
+	elif str(date) in html:
 		bot.sendMessage(chat_id = -1001299873599, text = "%d 예매오픈!" % date) 
+		confirm_date = date
 	else:
 		if(now.hour == 11 and now.minute == 59):
 			bot.sendMessage(chat_id = -1001299873599, text = "%d:%d 용아맥알리미 정상작동중!" % (now.hour, now.minute))
 		print(now)
 		print(date)
-	date = date + 1
+		date = date + 1
 
 
