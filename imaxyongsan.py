b772974504ce8e193185b5fc0b1d3398e0b4a1bf
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-17 15:25:09 +0900

    Update imaxyongsan.py

--- imaxyongsan.py
+++ imaxyongsan.py
@@ -12,7 +12,7 @@ my_token = '1136456443:AAHnhXqpT-NqUZiK909_5017pah7wJ6LQ04'
 bot = telegram.Bot(token = my_token)
 
 
-date = 20200830
+date = 20200901
 confirm_date = 20200900
 url='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=07&theatercode=0013&date=20200831&screencodes=&screenratingcode=02&regioncode=cf'
 response = requests.get(url)
@@ -43,6 +43,7 @@ while True:
 	else:
 		if(now.hour == 8 and now.minute == 10):
 			bot.sendMessage(chat_id = -1001299873599, text = "%d:%d 용아맥알리미 정상작동중!" % (now.hour, now.minute))
+			time.sleep(60)
 		print(now)
 		print('date =', date)
 		print('정상작동중')
