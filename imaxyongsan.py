53df40a79d6eef086c90e184965bd2b7cf060d1f
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2020-08-19 15:06:18 +0900

    Update imaxyongsan.py

--- imaxyongsan.py
+++ imaxyongsan.py
@@ -12,7 +12,7 @@ my_token = '1136456443:AAHnhXqpT-NqUZiK909_5017pah7wJ6LQ04'
 bot = telegram.Bot(token = my_token)
 
 
-date = 20200901
+date = 20200830
 confirm_date = 20200900
 url='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=07&theatercode=0013&date=20200831&screencodes=&screenratingcode=02&regioncode=cf'
 response = requests.get(url)
