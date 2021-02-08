4b1dac57806f6eb91113c3d40630f06f3b2f9696
dbstjr1500 <69731508+dbstjr1500@users.noreply.github.com>
2021-02-08 15:16:50 +0900

    Update Upbit_bot_RSI.py

--- Upbit_bot_RSI.py
+++ Upbit_bot_RSI.py
@@ -152,7 +152,7 @@ def account_info():
 	headers = {"Authorization": authorize_token}
 
 	res = requests.get(server_url + "/v1/accounts", headers=headers)
-
+	print(res.json())
 	return res.json()
 
 
